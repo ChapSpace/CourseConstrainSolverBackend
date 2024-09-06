aa62fca06010c5fe5de76c07e2636bc064230bfc
ericcui <ericcui2005@gmail.com>
2024-09-06 19:45:50 -0400

    Basic object-oriented course scheduling model

--- ideation/classes/Course.py
+++ ideation/classes/Course.py
@@ -1,18 +1,18 @@
 class Course:
     
-    def __init__(self, identifier: str, quarters_offered: list[str]) -> None:
-        self.__identifier = identifier
-        self.__quarters_offered = quarters_offered
+    def __init__(self, name: str, quarters: list[int]) -> None:
+        self.__name = name
+        self.__quarters = quarters
         
     # To string
     def __str__(self):
-        return f'{self.__identifier}'
+        return f'{self.__name}'
     
     # Accessors
-    def get_identifier(self):
-        return self.__identifier
-    def get_quarters_offered(self):
-        return self.__quarters_offered
+    def get_name(self):
+        return self.__name
+    def get_quarters(self):
+        return self.__quarters
     
     
     
--- ideation/main.py
+++ ideation/main.py
@@ -1,44 +1,23 @@
 from classes.Course import Course
 from classes.Program import Program
+from classes.Quarters import Quarters
 from z3 import *
 
 def main():
     
-    # Quarters assigned integer values
-    FRESH_FALL = 0
-    FRESH_WINTER = 1
-    FRESH_SPRING = 2
-    FRESH_SUMMER = 3
-    
-    C1 = Int('C1')
-    C2 = Int('C2')
-    C3 = Int('C3')
-    C4 = Int('C4')
-    C5 = Int('C5')
-    C6 = Int('C6')
-    C7 = Int('C7')
-    C8 = Int('C8')
+    degreeProgram = Program([Course('C1', [Quarters['FRESH_FALL'].value]),
+                            Course('C2', [Quarters['FRESH_FALL'].value, Quarters['FRESH_WINTER'].value]),
+                            Course('C3', [Quarters['FRESH_WINTER'].value, Quarters['FRESH_SPRING'].value]),
+                            Course('C4', [Quarters['FRESH_SPRING'].value])])
     
     s = Solver()
     
-    s.add(Or(C1 == FRESH_FALL))
-    s.add(Or(C2 == FRESH_FALL))
-    s.add(Or(C3 == FRESH_SPRING))
-    s.add(Or(C4 == FRESH_SUMMER))
-    s.add(Or(C5 == FRESH_FALL))
-    s.add(Or(C6 == FRESH_WINTER))
-    s.add(Or(C7 == FRESH_SPRING))
-    s.add(Or(C8 == FRESH_SUMMER))
-    
-    course_load = [If(C1 == q, 1, 0) + If(C2 == q, 1, 0) + If(C3 == q, 1, 0) + If(C4 == q, 1, 0) 
-                   + If(C4 == q, 1, 0)  + If(C5 == q, 1, 0)  + If(C6 == q, 1, 0)  + If(C7 == q, 1, 0) for q in range(4)]
-    for load in course_load:
-        s.add(load <= 2)
+    for course in degreeProgram.get_required_courses():
+        s.add(Or([Int(course.get_name()) == quarter for quarter in course.get_quarters()]))
     
     print(s.check())
-    
-    if s.check() == sat:
-        print(s.model())
+    print(s.model())
+   
     
 if __name__ == "__main__":
     main()
