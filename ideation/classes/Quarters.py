d36ea930782f94c105c3243bf209e9790b194aef
ericcui <ericcui2005@gmail.com>
2024-09-06 19:09:46 -0400

    Added ideation quarters enum

---
+++ ideation/classes/Quarters.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class Quarters(Enum):
+    
+    FRESH_FALL = 0
+    FRESH_WINTER = 1
+    FRESH_SPRING = 2
+    FRESH_SUMMER = 3
+    
+    SOPH_FALL = 4
+    SOPH_WINTER = 5
+    SOPH_SPRING = 6
+    SOPH_SUMMER = 7
+    
+    JUN_FALL = 8
+    JUN_WINTER = 9
+    JUN_SPRING = 10
+    JUN_SUMMER = 11
+    
+    SEN_FALL = 12
+    SEN_WINTER = 13
+    SEN_SPRING = 14
+    SEN_SUMMER = 15
