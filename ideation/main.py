5506e7727d6638e8dbb965457bc1310feef4a018
ericcui <ericcui2005@gmail.com>
2024-09-07 17:47:03 -0400

    Added quarter course number constraints and dynamic constraint loading

--- ideation/main.py
+++ ideation/main.py
@@ -5,17 +5,33 @@ from z3 import *
 
 def main():
     
-    degreeProgram = Program([Course('C1', [Quarters['FRESH_FALL'].value]),
-                            Course('C2', [Quarters['FRESH_FALL'].value, Quarters['FRESH_WINTER'].value]),
+    degreeProgram = Program([Course('C1', [Quarters['FRESH_FALL'].value, Quarters['FRESH_WINTER'].value]),
+                            Course('C2', [Quarters['FRESH_FALL'].value, Quarters['FRESH_SPRING'].value]),
                             Course('C3', [Quarters['FRESH_WINTER'].value, Quarters['FRESH_SPRING'].value]),
-                            Course('C4', [Quarters['FRESH_SPRING'].value])])
+                            Course('C4', [Quarters['FRESH_WINTER'].value, Quarters['FRESH_FALL'].value]),
+                            Course('C5', [Quarters['FRESH_SPRING'].value, Quarters['FRESH_FALL'].value]),
+                            Course('C6', [Quarters['FRESH_SPRING'].value, Quarters['FRESH_WINTER'].value])])
     
     s = Solver()
     
+    # Creating a dictionary of Int() z3 variables for subsequent access
+    # Dynamically creating z3 Int() variables from degree program dict
+    degreeCourses: dict[str: tuple(Int(), list[int])] = {}
     for course in degreeProgram.get_required_courses():
-        s.add(Or([Int(course.get_name()) == quarter for quarter in course.get_quarters()]))
+        degreeCourses[course.get_name()] = (Int(course.get_name()), course.get_quarters())
     
-     
+    print(degreeCourses)
+    
+    # Dynamically adding the constraint that each class must be taken at least once
+    for courseVarKey in degreeCourses:
+        courseVarPair = degreeCourses[courseVarKey]
+        s.add(Or([courseVarPair[0] == quarter for quarter in courseVarPair[1]])) 
+        
+    # Dynamically adding the constraint that we can have no more than 2 classes per quarter
+    for quarter in Quarters:
+        load = sum([If(courseVarPair[0] == quarter.value, 1, 0) for courseVarPair in degreeCourses.values()])
+        s.add(load <= 2)
+    
     
     print(s.check())
     print(s.model())
