eb12a13f6b1d4724fb5896b995164febfc58f33f
AliAhmad810 <hiimaliahmad@gmail.com>
2024-09-07 15:05:49 -0700

    Class methods for accessing member variables

--- src/course.py
+++ src/course.py
@@ -36,27 +36,76 @@ class Course:
         ug_reqs: Optional[List[GER]] = None,
         grading: Union[Grade, tuple[Grade, Grade]] = None
     ) -> None:
-        self.code = code
-        self.title = title
-        self.units = units if isinstance(units, int) or (isinstance(units, tuple) and len(units) == 2) else None
-        self.description = description
-        self.prereqs = prereqs or []
-        self.coreqs = coreqs or []
-        self.offered_quarters = offered_quarters or []
-        self.instructors = instructors or []
-        self.median_hrs = median_hrs
-        self.median_grade = median_grade
-        self.percent_A_A_plus = percent_A_A_plus
-        self.ug_reqs = ug_reqs
-        self.grading = grading
+        self._code = code
+        self._title = title
+        self._units = units if isinstance(units, int) or (isinstance(units, tuple) and len(units) == 2) else None
+        self._description = description
+        self._prereqs = prereqs or []
+        self._coreqs = coreqs or []
+        self._offered_quarters = offered_quarters or []
+        self._instructors = instructors or []
+        self._median_hrs = median_hrs
+        self._median_grade = median_grade
+        self._percent_A_A_plus = percent_A_A_plus
+        self._ug_reqs = ug_reqs
+        self._grading = grading
 
     def __str__(self) -> str:
         return f"{self.code}: {self.title}"
-
-    def get_prereqs(self):
+    
+    @property
+    def units(self) -> Union[int, tuple[int, int]]:
+        """Return the course units."""
+        return self._units
+    
+    @property
+    def prereqs(self) -> List['Course']:
         """Return the list of prerequisite courses."""
-        return self.prereqs
-
+        return self._prereqs
+    
     def add_prereq(self, prereq: 'Course') -> None:
         """Add a prerequisite to the course."""
-        self.prereqs.append(prereq)
+        if not isinstance(prereq, Course):
+            raise TypeError(f"Prerequisite must be a 'Course' object, but got {type(prereq).__name__}")
+        self._prereqs.append(prereq)
+
+    @property
+    def coreqs(self) -> List['Course']:
+        """Return the list of corequisite courses."""
+        return self._coreqs
+    
+    def add_coreq(self, coreq: 'Course') -> None:
+        """Add a corequisite to the course."""
+        if not isinstance(coreq, Course):
+            raise TypeError(f"Corequisite must be a 'Course' object, but got {type(prereq).__name__}")
+        self._coreqs.append(coreq)
+
+    @property
+    def offered_quarters(self) -> List[Quarter]:
+        """Return the quarters the course is offered."""
+        return self._offered_quarters
+    
+    @property
+    def instructors(self) -> List[List[str]]:
+        """Return the list of instructors per quarter."""
+        return self._instructors
+    
+    @property
+    def median_hrs(self) -> Union[int, float]:
+        """Return the median hours required."""
+        return self._median_hrs
+    
+    @property
+    def median_grade(self) -> Optional[Grade]:
+        """Return the median grade or None."""
+        return self._median_grade
+    
+    @property
+    def percent_A_A_plus(self) -> Optional[Union[int, float]]:
+        """Return the percentage of A/A+ grades or None."""
+        return self._percent_A_A_plus
+    
+    @property
+    def ug_reqs(self) -> Optional[List[GER]]:
+        """Return the undergraduate requirements or None."""
+        return self._ug_reqs
