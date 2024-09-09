928ed7d5216791b0daa63806475e48cd736e3e03
ericcui <ericcui2005@gmail.com>
2024-09-09 16:57:44 -0400

    Comments

--- src/mock_run.py
+++ src/mock_run.py
@@ -6,19 +6,19 @@ from classes.components.enums import Quarter
 
 def main():
     
+    # Arranging constraint objects
     degreeProgram = Program(required_courses=[Course(code='C1', units=5, offered_quarters=[Quarter.FALL]),
                             Course(code='C2', units=5, offered_quarters=[Quarter.WINTER]),
                             Course(code='C3', units=5, offered_quarters=[Quarter.SPRING]),
                             Course(code='C4', units=5, offered_quarters=[Quarter.SUMMER])])
-    
     constrainProfile = Profile(max_quarter_units=5)
 
+    # Creating and configuring solver
     scheduleSolver = SolverConfig(program=degreeProgram, profile=constrainProfile)
-    
     scheduleSolver.add_required_courses_constraints()
     scheduleSolver.add_quarter_load_constraints()
     
-    
+    # Solving
     scheduleSolver.solve_constraints()
 
     
