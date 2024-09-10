a0352b08ebf204143860ffa52591b2b93f2e6133
ericcui <ericcui2005@gmail.com>
2024-09-10 16:12:43 -0400

    Added more tests

--- tests/solver_test.py
+++ tests/solver_test.py
@@ -6,8 +6,6 @@ from classes.components.enums import Quarter
 from z3 import sat, unsat
 
 
-
-
 def test_add_required_courses_constraints_sat():
     
     degreeProgram = Program(required_courses=[Course(code='C1', units=5, offered_quarters=[Quarter.FALL]),
@@ -52,5 +50,46 @@ def test_add_required_courses_constraints_one_per_quarter_unsat():
     scheduleSolver.add_quarter_load_constraints()
     
     assert scheduleSolver.check_solvable() == unsat
+
+def test_add_required_courses_constraints_multiple_per_quarter_sat():
+    
+    degreeProgram = Program(required_courses=[Course(code='C1', units=5, offered_quarters=[Quarter.FALL]),
+                            Course(code='C2', units=5, offered_quarters=[Quarter.WINTER]),
+                            Course(code='C3', units=5, offered_quarters=[Quarter.SPRING]),
+                            Course(code='C4', units=5, offered_quarters=[Quarter.SUMMER]),
+                            Course(code='C5', units=5, offered_quarters=[Quarter.FALL]),
+                            Course(code='C6', units=5, offered_quarters=[Quarter.WINTER]),
+                            Course(code='C7', units=5, offered_quarters=[Quarter.SPRING]),
+                            Course(code='C8', units=5, offered_quarters=[Quarter.SUMMER])])
+    
+    constrainProfile = Profile(max_quarter_units=10)
+    
+    # Creating and configuring solver
+    scheduleSolver = SolverConfig(program=degreeProgram, profile=constrainProfile)
+    scheduleSolver.add_required_courses_constraints()
+    scheduleSolver.add_quarter_load_constraints()
+    
+    assert scheduleSolver.check_solvable() == sat
+    
+def test_add_required_courses_constraints_multiple_per_quarter_unsat():
+    
+    degreeProgram = Program(required_courses=[Course(code='C1', units=5, offered_quarters=[Quarter.FALL]),
+                            Course(code='C2', units=5, offered_quarters=[Quarter.WINTER]),
+                            Course(code='C3', units=5, offered_quarters=[Quarter.SPRING]),
+                            Course(code='C4', units=5, offered_quarters=[Quarter.SUMMER]),
+                            Course(code='C5', units=5, offered_quarters=[Quarter.FALL]),
+                            Course(code='C6', units=5, offered_quarters=[Quarter.WINTER]),
+                            Course(code='C7', units=6, offered_quarters=[Quarter.SPRING]),
+                            Course(code='C8', units=5, offered_quarters=[Quarter.SUMMER])])
+    
+    constrainProfile = Profile(max_quarter_units=10)
+    
+    # Creating and configuring solver
+    scheduleSolver = SolverConfig(program=degreeProgram, profile=constrainProfile)
+    scheduleSolver.add_required_courses_constraints()
+    scheduleSolver.add_quarter_load_constraints()
+    
+    assert scheduleSolver.check_solvable() == unsat
+    
     
     
