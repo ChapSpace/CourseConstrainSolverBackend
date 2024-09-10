9d6025b52f734b3332ff29b84ecda33e7fc5960e
ericcui <ericcui2005@gmail.com>
2024-09-10 15:54:51 -0400

    Deleted unused files and finalized test configuration

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+import sys
+import os
+
+# Add the src/ directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
---
+++ tests/solver_test.py
@@ -0,0 +1,8 @@
+from classes.solver_config import SolverConfig
+from classes.constrain.program import Program
+from classes.constrain.profile import Profile
+from classes.components.course import Course
+from classes.components.enums import Quarter
+
+def test():
+    assert 1 == 1
