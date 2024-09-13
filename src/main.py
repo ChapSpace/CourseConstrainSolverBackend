48f3f3297dc04110d4e4c66f2493ef065a66193c
AliAhmad810 <hiimaliahmad@gmail.com>
2024-09-12 18:33:53 -0700

    Clean up main.py

--- src/main.py
+++ src/main.py
@@ -40,28 +40,6 @@ def main():
     else:
         print("No valid schedule found")
 
-
-    """
-    # Arranging constraint objects
-    degreeProgram = Program(
-        required_courses=[
-            Course(code='C1', units=5, offered_quarters=[Quarter.FALL]),
-            Course(code='C2', units=5, offered_quarters=[Quarter.WINTER]),
-            Course(code='C3', units=5, offered_quarters=[Quarter.SPRING]),
-            Course(code='C4', units=5, offered_quarters=[Quarter.SUMMER])
-        ]
-    )
-    constrainProfile = Profile(max_quarter_units=5)
-
-    # Creating and configuring solver
-    scheduleSolver = SolverConfig(program=degreeProgram, profile=constrainProfile)
-    scheduleSolver.add_required_courses_constraints()
-    scheduleSolver.add_quarter_load_constraints()
-    
-    # Solving
-    print(scheduleSolver.check_solvable())
-    """
-
     
 if __name__ == '__main__':
     main()
