333d12771a556b24e2c6bb919060c8b3ae959ce8
AliAhmad810 <hiimaliahmad@gmail.com>
2024-09-13 12:38:07 -0700

    refactor profile class to call observers when things change

--- src/classes/constrain/profile.py
+++ src/classes/constrain/profile.py
@@ -9,31 +9,31 @@ class Profile:
         self._min_quarter_units = min_quarter_units
 
         self._observers = []
-        
-    def add_observer(self, observer):
-        self._observers.append(observer)
-    
-    def remove_observer(self, observer):
-        self._observers.remove(observer)
-    
-    def _notify_observers(self):
-        for observer in self._observers:
-            observer()
 
     @property
     def max_quarter_units(self) -> int:
         return self._max_quarter_units
     
-    @max_quarter_units.setter
-    def max_quarter_units(self, value: int):
-        self._max_quarter_units = value
-        self._notify_observers("max_quarter_units")
-
     @property
     def min_quarter_units(self) -> int:
         return self._min_quarter_units
     
+    @max_quarter_units.setter
+    def max_quarter_units(self, value: int):
+        self._max_quarter_units = value
+        self._notify_observers()
+    
     @min_quarter_units.setter
     def min_quarter_units(self, value: int):
         self._min_quarter_units = value
-        self._notify_observers("min_quarter_units")
+        self._notify_observers()
+
+    def add_observer(self, observer):
+        self._observers.append(observer)
+    
+    def remove_observer(self, observer):
+        self._observers.remove(observer)
+
+    def _notify_observers(self):
+        for observer in self._observers:
+            observer()
