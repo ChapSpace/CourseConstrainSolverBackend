ff582cd6432bfe5aee0a8a13f6d3951d78710dec
ericcui <ericcui2005@gmail.com>
2024-09-24 21:39:26 -0700

    Added to_dict() and from_dict() methods to new Pool class and modified same functions in program

--- src/classes/components/pool.py
+++ src/classes/components/pool.py
@@ -1,4 +1,4 @@
-from typing import Union, List
+from typing import Union, List, Dict, Any
 from classes.components.course import Course
 
 class Pool: 
@@ -6,7 +6,7 @@ class Pool:
     Represents a degree program requirement whereby a certain number of requirements of a certain type must be fulfilled
     
     Attributes:
-        type (str): The type of objects in the collection. Can be of type Course or another Pool
+        type (str): The type of objects in the collection. Can be either "Course" or "Pool"
         objects (List[Union[Course, 'Pool']]): The actual requirements in the collection
         num_required (int): The number of object requirements that must be fulfilled (Ex: Fulfill at least two courses from the collection)
     """
@@ -14,6 +14,30 @@ class Pool:
         self._type = type
         self._objects = objects
         self._num_required = num_required
+        
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "type": self._type,
+            "objects": [obj.to_dict() for obj in self._objects],
+            "num_required": self._num_required
+        }
+    
+    def from_dict(cls, dict) -> 'Pool':
+        
+        obj_dicts = dict.get("objects")
+        objs = None
+        
+        if dict.get("type") == "Course":
+            objs = [Course.from_dict(course_dict) for course_dict in obj_dicts]
+        elif dict.get("type") == "Pool":
+            objs = [Pool.from_dict(pool_dict) for pool_dict in obj_dicts]
+            
+        return cls(
+            type=dict.get("type"),
+            objects=objs,
+            num_required=dict.get("num_required")
+        )
+        
     
     @property
     def type(self) -> str:
--- src/classes/constrain/program.py
+++ src/classes/constrain/program.py
@@ -1,30 +1,44 @@
 from classes.components.course import Course
+from classes.components.pool import Pool
 from typing import List, Dict, Any
 
 class Program:
-    """Degree program with required courses."""
-    def __init__(self, id: str, required_courses: List[Course]):
-        self._required_courses = required_courses
+    """
+    Degree program with program requirements.
+    
+    Attributes:
+        id (str): Describes the name of the program
+        required_courses (List[Course]): The courses required to complete the program
+        pools (List[Pool]): The pool requirements that must be satisfied to complete the program
+    
+    """
+    def __init__(self, id: str, required_courses: List[Course], pools: List[Pool]):
         self._id = id
+        self._required_courses = required_courses
+        self._pools = pools
     
     def to_dict(self) -> Dict[str, Any]:
         return {
             "required_courses": [course.to_dict() for course in self._required_courses],
-            "id": self._id
+            "id": self._id,
+            "pools": [pool.to_dict() for pool in self._pools]
         }
     
     @classmethod
     def from_dict(cls, dict) -> 'Program':
         return cls(
             required_courses=[Course.from_dict(course_dict) for course_dict in dict.get("required_courses")] if dict.get("required_courses") != None else [],
-            id=dict.get("id")
+            id=dict.get("id"),
+            pools=[Pool.from_dict(pool_dict) for pool_dict in dict.get("pools")] if dict.get("pools") != None else []
         )
     
     @property
     def id(self) -> str:
         return self._id
-    
     @property
     def required_courses(self) -> List[Course]:
         return self._required_courses
+    @property
+    def pools(self) -> List[Pool]:
+        return self._pools
 
