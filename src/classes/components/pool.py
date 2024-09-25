5d3e5ac83695bbff8b99a7fed6546ec179245877
ericcui <ericcui2005@gmail.com>
2024-09-24 21:51:02 -0700

    Changed Pool class list of objects to either be a list of course codes or a list of Pools as opposed to a list of Course objects

--- src/classes/components/pool.py
+++ src/classes/components/pool.py
@@ -7,10 +7,10 @@ class Pool:
     
     Attributes:
         type (str): The type of objects in the collection. Can be either "Course" or "Pool"
-        objects (List[Union[Course, 'Pool']]): The actual requirements in the collection
+        objects (List[Union[str, 'Pool']]): The actual requirements in the collection. Can either be a list of course codes or a collection of pools
         num_required (int): The number of object requirements that must be fulfilled (Ex: Fulfill at least two courses from the collection)
     """
-    def __init__(self, type: str, objects: List[Union[Course, 'Pool']], num_required: int):
+    def __init__(self, type: str, objects: List[Union[str, 'Pool']], num_required: int):
         self._type = type
         self._objects = objects
         self._num_required = num_required
@@ -18,26 +18,16 @@ class Pool:
     def to_dict(self) -> Dict[str, Any]:
         return {
             "type": self._type,
-            "objects": [obj.to_dict() for obj in self._objects],
+            "objects": [obj.to_dict() for obj in self._objects] if self._type == "Course" else self._objects,
             "num_required": self._num_required
         }
     
-    def from_dict(cls, dict) -> 'Pool':
-        
-        obj_dicts = dict.get("objects")
-        objs = None
-        
-        if dict.get("type") == "Course":
-            objs = [Course.from_dict(course_dict) for course_dict in obj_dicts]
-        elif dict.get("type") == "Pool":
-            objs = [Pool.from_dict(pool_dict) for pool_dict in obj_dicts]
-            
+    def from_dict(cls, dict) -> 'Pool':       
         return cls(
             type=dict.get("type"),
-            objects=objs,
+            objects=[Course.from_dict(course_dict) for course_dict in dict.get("objects")] if dict.get("type") == "Course" else dict.get("objects"),
             num_required=dict.get("num_required")
         )
-        
     
     @property
     def type(self) -> str:
